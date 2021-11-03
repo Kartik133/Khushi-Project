62e3b3d5b063bbef79bc7d530b82a82d16767fad
Kartik133 <75470483+Kartik133@users.noreply.github.com>
2021-11-03 10:20:31 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -10,4 +10,12 @@ function setup() {
 
 function draw() {
   background(255,255,191);
+  
+  textAlign(CENTER);
+  textFont(a);
+  textSize(20);
+  noStroke();
+  fill(0);
+  text("GREEN VALLEY",width/2,100);
+  text("PUBLIC SCHOOL",width/2,150);
 }
