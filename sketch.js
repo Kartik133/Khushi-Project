c5ab160229a5cddf8d7f38898ad7a7b08582b200
Kartik133 <75470483+Kartik133@users.noreply.github.com>
2021-11-03 12:23:41 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -17,7 +17,7 @@ function draw() {
   noStroke();
   fill(0);
   text("ACKNOWLEDGEMENT",width/2,300);
-  textSize(100);
+  textSize(90);
   text("It would be my atmost pleasure to express my",width/2,550);
   text("sincere thanks to the honourable director sir Mr.",width/2,800);
   text("G.S.BHATIYA, principal maam Mrs. RAVINDRA",width/2,1050); 
